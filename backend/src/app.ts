e33c2d4d033392ab06ea45f28b6b00e056391d5e
Ítalo Alves <italooko@gmail.com>
2022-08-28 17:09:42 -0300

    Modifica app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,38 +1,41 @@
+import { getAllJogadores } from "./services/jogador";
+
 //Função que recebe inputs
-import verifyUser from "./verifyuser";
-const input = require('prompt-sync')({sigint: true});
+const input = require('prompt-sync')({ sigint: true });
+
 let game = true;
 
 function verify() {
 
-    verifyUser()
-   
+    getAllJogadores()
+
 }
-    //Checar se há jogadores na tabela personagem
-    //Se tiver jogador, mostrar lista para selecionar personagem
-    /*
-    * Selecione o personagem que deseja utilizar, ou digite 0 para criar um personagem novo
-    *
-    * 1) Oswaldo
-    * 2) Adoniran
-    * 3) louco
-    * 
-    * 0
-    */
 
-    //Se não tiver jogador, mostrar tela para criação do personagem
-    /*
-    * Nome = Tritao
-    * lista das classes
-    * lista das racas
-    * 
-    */
+//Checar se há jogadores na tabela personagem
+//Se tiver jogador, mostrar lista para selecionar personagem
+/*
+* Selecione o personagem que deseja utilizar, ou digite 0 para criar um personagem novo
+*
+* 1) Oswaldo
+* 2) Adoniran
+* 3) louco
+* 
+* 0
+*/
 
-while(game){
-    
+//Se não tiver jogador, mostrar tela para criação do personagem
+/*
+* Nome = Tritao
+* lista das classes
+* lista das racas
+* 
+*/
+
+while (game) {
+
     let x = input();
     verify()
-    if(x == 'a'){
+    if (x == 'a') {
         game = false;
     }
 }
