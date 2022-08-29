a17d3603e09d81852ab84f87392b3323d64c3268
Ítalo Alves <italooko@gmail.com>
2022-08-28 22:41:03 -0300

    Adiciona textos de telas

---
+++ backend/src/telas.ts
@@ -0,0 +1,29 @@
+
+export function menu() {
+    console.clear();
+    console.log('');
+    console.log('');
+    console.log(' __       __  __    __  __    __   ______   __    __  __    __  ______  __    __ \r\n|  \\     \/  \\|  \\  |  \\|  \\  |  \\ \/      \\ |  \\  |  \\|  \\  \/  \\|      \\|  \\  |  \\\r\n| $$\\   \/  $$| $$  | $$| $$\\ | $$|  $$$$$$\\| $$  | $$| $$ \/  $$ \\$$$$$$| $$\\ | $$\r\n| $$$\\ \/  $$$| $$  | $$| $$$\\| $$| $$   \\$$| $$__| $$| $$\/  $$   | $$  | $$$\\| $$\r\n| $$$$\\  $$$$| $$  | $$| $$$$\\ $$| $$      | $$    $$| $$  $$    | $$  | $$$$\\ $$\r\n| $$\\$$ $$ $$| $$  | $$| $$\\$$ $$| $$   __ | $$$$$$$$| $$$$$\\    | $$  | $$\\$$ $$\r\n| $$ \\$$$| $$| $$__\/ $$| $$ \\$$$$| $$__\/  \\| $$  | $$| $$ \\$$\\  _| $$_ | $$ \\$$$$\r\n| $$  \\$ | $$ \\$$    $$| $$  \\$$$ \\$$    $$| $$  | $$| $$  \\$$\\|   $$ \\| $$  \\$$$\r\n \\$$      \\$$  \\$$$$$$  \\$$   \\$$  \\$$$$$$  \\$$   \\$$ \\$$   \\$$ \\$$$$$$ \\$$   \\$$');
+    console.log('');
+    console.log('');
+    console.log('[1] Novo jogo');
+    console.log('[2] Continuar');
+    console.log('');
+    console.log('');
+}
+
+export function novoJogo() {
+    console.clear();
+    console.log('');
+    console.log('');
+    console.log('Selecione o personagem que deseja utilizar, ou digite 0 para criar um personagem novo:');
+    console.log('');
+    console.log('[1] Oswaldo');
+    console.log('[2] Adoniran');
+    console.log('[3] Louco');
+    console.log('');
+    console.log('[4] Novo personagem');
+    console.log('[0] Voltar');
+    console.log('');
+    console.log('');
+}
